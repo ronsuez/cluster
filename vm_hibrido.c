cca40f04cb8d4785f42c5983ca6ca22220b041c1
Ronald Suez <rsuez@4geeks.com.ve>
2014-07-02 18:05:11 -0430

    Matriz

--- vm_hibrido.c
+++ vm_hibrido.c
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
         for ( i = 0 ; i < n ; i++)
             for (j = 0; j < m ; j++)
             {
-                b[j + (i*m)] = (i+1)*j;
+                b[j + (i*m)] = (i)*(j+1);
                 printf("mat[%d]= %.3lf\n",j + (i*m),b[j + (i*m)]);
 
             }
