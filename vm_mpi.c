1ddbbcce86bf4fcddeb945073bd1c4685d75fcf8
Daniel Iturriza <diturriza17@gmail.com>
2014-07-03 09:08:31 -0430

    vm_mpi divide columnas en partes iguales a cantidad de procesos. Version da error al tener columnas mayores a 80 , CONSIDERAR COLUMNAS MULTIPLOS DE CANTIDAD DE PROCESOS

--- vm_mpi.c
+++ vm_mpi.c
@@ -11,17 +11,18 @@ int main(int argc, char **argv)
 {
 	double	a[N],				/*Vector A*/
 			b[M+(N*M)],			/*Matriz B*/
-			c[N];				/*Vector Fila resultante C*/			
+			c[N],				/*Vector Fila resultante C*/			
+			vaux[N];
 	
-	double	s;		
+	double	s,col_proc;
 	
 	int 	size, 				/*Cantidad de procesos*/
 			my_rank,			/*Numero de proceso*/
 			num_procesos,		/*Cantidad de procesos 'workers'*/
-			col_proc,			/*columnas por proceso*/
-			col_rest,			/*Columnas restantes*/
+						/*columnas por proceso*/
+			aux,			/*Columnas restantes*/
 			m ,n,				/*Fila y columna*/
-			i,j,k;				/*Variables de control*/
+			i,j,k,li,ls;				/*Variables de control*/
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
@@ -45,8 +46,8 @@ int main(int argc, char **argv)
 			}
 
 		}*/
-		m = 50;	/* cantidad de filas */
-		n = num_procesos;
+				//m = 50;	/* cantidad de filas */
+		//n = num_procesos;
 
 		/*for(i=0; i<m; i++){
 			printf("vec[%d]= ", i);
@@ -59,38 +60,58 @@ int main(int argc, char **argv)
 				scanf("%lf", &b[j + (i*m)]);
 			}
 		}*/
-        for (i = 0 ; i < m; i++)
-        {
-            a[i] = i+1; 
-            printf("vec[%d]= %.3lf\n",i,a[i]);
-        }
-        
-        for ( i = 0 ; i < n ; i++)
-            for (j = 0; j < m ; j++)
-            {
-                b[j + (i*m)] = i*(j+1);
-                printf("mat[%d]= %.3lf\n", j+(i*m),b[j+(i*m)]);
+		do{
+			printf("Procesos a Ejecutar %d\n",num_procesos);
+			printf("Indica filas y columnas\n");
+			scanf("%d %d",&m,&n);
 
-            }
+			for (i = 0 ; i < m; i++)
+			{
+				a[i] = i+1; 
+				//printf("vec[%d]= %.3lf\n",i,a[i]);
+			}
+			
+			for ( i = 0 ; i < n ; i++)
+				for (j = 0; j < m ; j++)
+				{
+					b[j + (i*m)] = i*(j+1);
+				 //printf("mat[%d]= %.3lf\n", j+(i*m),b[j+(i*m)]);
+
+				}
+
+			col_proc = n / num_procesos;
+			printf("\n**********%.0lf\n************\n",col_proc);
+		}while (n%num_procesos != 0);
 		
+		getchar();
 		/*Enviar datos de entrada a los procesos*/
 		
 		for (i = 1 ; i <= num_procesos ; i++)
 		{			
-			printf("Soy el proceso %d, estoy enviando a P%d datos para su tarea\n",my_rank, i);
+			//printf("Soy el proceso %d, estoy enviando a P%d datos para su tarea\n",my_rank, i);
 			MPI_Send(&m, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 			MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 			MPI_Send(&a, m, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 			MPI_Send(&b, (m*n), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&col_proc, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 		}
 		
 		for (i = 1 ; i <=num_procesos ; i++)
 		{
-			MPI_Recv(&c[i-1], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			printf("Soy el proceso %d, estoy recibiendo de P%d resultados\n",my_rank, i);
+			/*MPI_Recv(&li, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&ls, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);*/
+			MPI_Recv(&aux, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&vaux, aux, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			
+			printf("Soy el proceso %d, ini %.0lf fin %.0lf\n",my_rank,(i-1)*col_proc, (i)*col_proc);
+			for ( j = ((i-1)*col_proc) ; j < ((i)*col_proc) ; j++)
+			{
+				c[j] = vaux[j-(int)((i-1)*col_proc)];
+			}
+			//printf("Soy el proceso %d, estoy recibiendo de P%d resultados\n",my_rank, i);
 		}
 		
-		printf("Vector [");
+		printf("\n\nVector [");
 		
 		for (i= 0 ; i < n ; i++)
 		{
@@ -105,17 +126,31 @@ int main(int argc, char **argv)
 		MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MPI_Recv(&a, m, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MPI_Recv(&b, (m*n), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&col_proc, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		
-		printf("Soy el proceso %d, estoy recibiendo de P%d \n",my_rank, 0);
-		int i, j;
-		double s = 0.0;
-		i = my_rank - 1;
-		for( j = 0 ; j < m; j++ )
+		//printf("Soy el proceso %d, estoy recibiendo de P%d \n",my_rank, 0);
+		int i, j,cont;
+		double  min, max;
+		
+		min = (my_rank - 1) * col_proc;
+		max = my_rank * col_proc;
+		
+		
+		printf("Soy el proceso %d, ini %.0lf fin %.0lf\n",my_rank, min, max);
+		double s[(int)(max-min)];
+		cont = 0;
+		for (i = min ; i < max ; i++)
 		{
-			s += a[j] * b[ (i*m) + j ];		
+			for( j = 0 ; j < m; j++ )
+			{
+				s[cont] += a[j] * b[ (i*m) + j ];		
+			}
+			cont++;
 		}
-		printf("Soy el proceso %d, enviando el dato %.3lf \n",my_rank, s);
-		MPI_Send(&s, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);	
+		cont++;
+		//printf("Soy el proceso %d, enviando el dato\n",my_rank);
+		MPI_Send(&cont, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&s, cont, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		
 	}	
 	
