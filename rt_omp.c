cc357951273b8d30f7b017cb11578683c745fac8
oscar <osalbr.14@gmail.com>
2014-07-03 07:42:48 -0430

    agregue rt_omp.c

--- rt_omp.c
+++ rt_omp.c
@@ -1,53 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
- 
+
 void Trap(double a, double b, int n, double* global_result_p);
 double f(double x);
- 
+
 int main(int argc, char* argv[]) {
-    double global_result = 0.0;
-    double a, b;
-    int n;
-    int thread_count = strtol(argv[1], NULL, 10);
-     
-    do{
-        printf("Introduce a, b y n que sea divisible por cantida de hilos (%d) \n",thread_count);
-        scanf("%lf %lf %d", &a, &b, &n);
-    }while(n%thread_count!=0);
- 
-    #pragma omp parallel num_threads(thread_count)
-        Trap(a, b, n, &global_result);
- 
-    printf("Integral desde %.1f a %.1f = %.4lf\n", a, b, global_result);
- 
+	double global_result = 0.0;
+	double a, b;
+	int n;
+	int thread_count = strtol(argv[1], NULL, 10);
+	
+	do{
+		printf("Introduce a, b y n que sea divisible por cantida de hilos (%d) \n",thread_count);
+		scanf("%lf %lf %d", &a, &b, &n);
+	}while(n%thread_count!=0);
+
+	#pragma omp parallel num_threads(thread_count)
+		Trap(a, b, n, &global_result);
+
+	printf("Integral desde %.1f a %.1f = %.4lf\n", a, b, global_result);
+
 }
- 
+
 double f(double x){
-    return (double) (x*x)+4;    
+	return (double) (x*x)+4;	
 }
- 
+
 void Trap(double a, double b, int n, double* global_result_p) {
      double h, x, my_result;
      double local_a, local_b;
      int i, local_n;
      int my_rank = omp_get_thread_num();
      int thread_count = omp_get_num_threads();
- 
+
      h = (b-a)/n;
      local_n = n/thread_count;
      local_a = a + my_rank*local_n*h;
      local_b = local_a + local_n*h;
-      
-      
+     
+     
      my_result = (f(local_a) + f(local_b))/2.0;
-      
+     
      for (i = 1; i <= local_n-1; i++) {
          x = local_a + i*h;
          my_result += f(x);
      }
      my_result = my_result*h;
- 
-    #pragma omp critical
-        *global_result_p += my_result;
+
+	#pragma omp critical
+		*global_result_p += my_result;
 }
